8f309494107aa7d5c3cfd18ef43bc18415941068
momeemt <momiimt@gmail.com>
2020-04-17 21:40:40 +0900

    ✨ TypeScriptに記述し直した buildが通らなかったので

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@
     <button @click="outputJson">JSONを吐き出す</button>
     <div class="fileDrop">
       <input
-        type="file"
-        id="inputFile"
-        value=""
-        @change="onFileChange"
-        accept="video/mp4"
+              type="file"
+              id="inputFile"
+              value=""
+              @change="onFileChange"
+              accept="video/mp4"
       />
     </div>
     <MakeComponents />
@@ -16,62 +16,65 @@
 </template>
 
 <script lang="ts">
-  import MakeComponents from "./components/MakeComponents.vue";
-export default {
-  name: 'App',
+import { Component, Vue } from 'vue-property-decorator'
+import MakeComponents from './components/MakeComponents.vue'
+
+@Component({
   components: {
     MakeComponents
-  },
-  data () {
-    return {
-      targetFilePath: ''
-    }
-  },
-  methods: {
-    onFileChange(event) {
-      const files = event.target.files || event.dataTransfer.files
-      const filePath = files[0].path
-      this.targetFilePath = filePath
-    },
-    outputJson() {
-      const Dialog = require('electron').remote.dialog;
-      if (this.targetFilePath === '') {
-        Dialog.showErrorBox('出力できませんでした', 'ファイルが設定されていません')
-      } else {
-        const outputData = {
-          info: {
-            'target_movie': this.targetFilePath,
-            'task_count': 0
-          }
+  }
+})
+
+export default class App extends Vue {
+  public targetFilePath = ''
+
+  public onFileChange (event: any) {
+    const files = event.target.files || event.dataTransfer.files
+    const filePath = files[0].path
+    this.targetFilePath = filePath
+  }
+
+  public outputJson () {
+    const Dialog = require('electron').remote.dialog
+    if (this.targetFilePath === '') {
+      Dialog.showErrorBox('出力できませんでした', 'ファイルが設定されていません')
+    } else {
+      const outputData = {
+        info: {
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          target_movie: this.targetFilePath,
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          task_count: 0
         }
-        const outputDataJson = JSON.stringify(outputData, null, 2)
-        const fs = require('fs')
-        fs.writeFile(`/Users/momiyama/Desktop/JSON/example1.json`, outputDataJson, (error) => {
-          if (error !== null) {
-            console.log(error)
-          }
-        })
       }
+      const outputDataJson = JSON.stringify(outputData, null, 2)
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const fs = require('fs')
+      fs.writeFile('/Users/momiyama/Desktop/JSON/example1.json', outputDataJson, (error: any) => {
+        if (error !== null) {
+          console.log(error)
+        }
+      })
     }
   }
 }
 </script>
 
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  .fileDrop {
-    height: 500px;
-    border: red 2px dotted;
-  }
-  #inputFile {
-    width: 100%;
-    height: 100%;
-  }
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.fileDrop {
+  height: 500px;
+  border: red 2px dotted;
+}
+#inputFile {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- src/components/MakeComponents.vue
+++ src/components/MakeComponents.vue
@@ -15,80 +15,80 @@
   </div>
 </template>
 
-<script>
-  export default {
-    name: "MakeComponents.vue",
-    data() {
-      return {
-        ratio: 1,
-        dx: 0,
-        dy: 0,
-        property: {
-          x: 10,
-          y: 10,
-          w: 100,
-          h: 100,
-          color: 'red'
-        },
-        isMove: false,
-        beforeMouseX: null,
-        beforeMouseY: null
-      }
-    },
-    mounted () {
-      console.log('MOUNT LISTENER ON')
-      document.addEventListener('mouseup', this.mouseUp)
-      document.addEventListener('mousemove', this.mouseMove)
-    },
-    beforeDestroy () {
-      console.log('MOUNT LISTENER OFF')
-      document.removeEventListener('mouseup', this.mouseUp)
-      document.removeEventListener('mousemove', this.mouseMove)
-    },
-    methods: {
-      openContextMenu () {
-        const remote = require('electron').remote
-        const Menu = remote.Menu
-        const MenuItem = remote.MenuItem
+<script lang>
+export default {
+  name: 'MakeComponents.vue',
+  data () {
+    return {
+      ratio: 1,
+      dx: 0,
+      dy: 0,
+      property: {
+        x: 10,
+        y: 10,
+        w: 100,
+        h: 100,
+        color: 'red'
+      },
+      isMove: false,
+      beforeMouseX: null,
+      beforeMouseY: null
+    }
+  },
+  mounted () {
+    console.log('MOUNT LISTENER ON')
+    document.addEventListener('mouseup', this.mouseUp)
+    document.addEventListener('mousemove', this.mouseMove)
+  },
+  beforeDestroy () {
+    console.log('MOUNT LISTENER OFF')
+    document.removeEventListener('mouseup', this.mouseUp)
+    document.removeEventListener('mousemove', this.mouseMove)
+  },
+  methods: {
+    openContextMenu () {
+      const remote = require('electron').remote
+      const Menu = remote.Menu
+      const MenuItem = remote.MenuItem
 
-        let menu = new Menu()
-        menu.append(new MenuItem({ label: 'MenuItem1', click: function() { console.log('item 1 clicked'); } }))
-        menu.append(new MenuItem({ type: 'separator' }))
-        menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }))
+      const menu = new Menu()
+      menu.append(new MenuItem({ label: 'MenuItem1', click: function () { console.log('item 1 clicked') } }))
+      menu.append(new MenuItem({ type: 'separator' }))
+      menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }))
 
-        window.addEventListener('contextmenu', function (e) {
-          e.preventDefault()
-          menu.popup(remote.getCurrentWindow())
-        }, false)
-      },
-      move (e) {
-        this.isMove = true
+      window.addEventListener('contextmenu', function (e) {
         e.preventDefault()
-      },
-      mouseUp (e) {
-        this.isMove = false
-        this.beforeMouseX = null
-        this.beforeMouseY = null
-        e.preventDefault()
-      },
-      mouseMove (e) {
-        if (!this.isMove) return
-        const mouseX = e.offsetX * this.ratio + this.dx
-        const mouseY = e.offsetY * this.ratio + this.dy
-        let dx = 0
-        let dy = 0
-        if (this.beforeMouseX && this.beforeMouseY) {
-          dx = mouseX - this.beforeMouseX
-          dy = mouseY - this.beforeMouseY
-        }
-        this.beforeMouseX = mouseX
-        this.beforeMouseY = mouseY
-        const tempX = dx + Number(this.property.x)
-        const tempY = dy + Number(this.property.y)
-        if (tempX > 0) this.property.x = tempX
-        if (tempY > 0) this.property.y = tempY
-        e.preventDefault()
+        menu.popup(remote.getCurrentWindow())
+      }, false)
+    },
+    move (e) {
+      this.isMove = true
+      e.preventDefault()
+    },
+    mouseUp (e) {
+      this.isMove = false
+      this.beforeMouseX = null
+      this.beforeMouseY = null
+      e.preventDefault()
+    },
+    mouseMove (e) {
+      if (!this.isMove) return
+      const mouseX = e.offsetX * this.ratio + this.dx
+      const mouseY = e.offsetY * this.ratio + this.dy
+      let dx = 0
+      let dy = 0
+      if (this.beforeMouseX && this.beforeMouseY) {
+        dx = mouseX - this.beforeMouseX
+        dy = mouseY - this.beforeMouseY
       }
+      this.beforeMouseX = mouseX
+      this.beforeMouseY = mouseY
+      const tempX = dx + Number(this.property.x)
+      const tempY = dy + Number(this.property.y)
+      if (tempX > 0) this.property.x = tempX
+      if (tempY > 0) this.property.y = tempY
+      e.preventDefault()
     }
   }
+}
 </script>
