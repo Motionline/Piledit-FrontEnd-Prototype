e92a7ac2cd68b6c0ed0b4659c849bef3340685a0
Momeemt <momiimt@gmail.com>
2020-08-05 22:33:43 +0900

    ✨ コンポーネント定義ブロック・イベントブロック基盤の実装

---
+++ src/components/Organisms/DefinitionComponentBlock.vue
@@ -0,0 +1,73 @@
+<template>
+  <EventBlockBase
+    :blockUniqueKey="blockUniqueKey"
+    :x="x"
+    :y="y"
+    :showShadow="showShadow"
+    :strokeColor="strokeColor"
+    :fillColor="fillColor"
+    :width="width"
+    @stopDragging="stopDragging($event)"
+    @updatePosition="updatePosition($event)"
+    @removeBlock="removeBlock($event)"
+  >
+    <SVGText x="10" y="45">
+      コンポーネント定義
+    </SVGText>
+  </EventBlockBase>
+</template>
+
+<script>
+import SVGText from '@/components/Atoms/SVGText'
+import EventBlockBase from '@/components/Molecules/EventBlockBase'
+export default {
+  name: 'DefinitionComponentBlock',
+  components: {
+    SVGText,
+    EventBlockBase
+  },
+  props: {
+    blockUniqueKey: {
+      type: String,
+      required: true
+    },
+    x: {
+      type: Number,
+      required: true
+    },
+    y: {
+      type: Number,
+      required: true
+    },
+    showShadow: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data () {
+    return {
+      selectFilePath: '',
+      strokeColor: '#e9bc00',
+      fillColor: '#fcc800',
+      width: '250'
+    }
+  },
+  methods: {
+    stopDragging (event) {
+      this.$emit('stopDragging', event)
+    },
+    updatePosition (event) {
+      this.$emit('updatePosition', event)
+    },
+    removeBlock (event) {
+      this.$emit('removeBlock', event)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .openDirectoryButton .v-btn__content {
+    position: unset !important;
+  }
+</style>
